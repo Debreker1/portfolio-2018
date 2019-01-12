20948669d94d5caf8ea7be29862fa74536bcb5e5
Ryan Graute <rrgraute@gmail.com>
2019-01-12 20:12:35 +0100

    add projects

--- src/Models.ts
+++ src/Models.ts
@@ -14,9 +14,27 @@ export class Project {
 }
 
 export const projects = [
+    // new Project(
+    // "Stormwatch",
+    // "Deze website is opgezet met behulp van wordpress. Deze site wordt gebruikt door een groep mensen in het spel Clash of Clans",
+    // "stormwatch.jpg",
+    // "http://stormwatch-clan.nl"),
     new Project(
-    "Stormwatch",
-    "Deze website is opgezet met behulp van wordpress. Deze site wordt gebruikt door een groep mensen in het spel Clash of Clans",
-    "stormwatch.jpg",
-    "http://stormwatch-clan.nl")
+        "Minigame",
+        "Een minigame geschreven in python voor het tweede project op het HRO",
+        "minigame.png",
+        "https://github.com/Debreker1/project2-minigame"
+    ),
+    new Project(
+        "Contact tracker app",
+        "Een mobiele applicatie voor het bijhouden hoe vaak jij je contactlenzen hebt gedragen",
+        "lenzen.png",
+        "https://github.com/Debreker1/contactsTrackerApp"
+    ),
+    new Project(
+        "Data visualizatie",
+        "Een project in het eerste jaar van Hogeschool Rotterdam om een applicatie te maken voor het visualiseren van diefstal",
+        "project3.png",
+        "https://github.com/Debreker1/project3"
+    )
 ]
